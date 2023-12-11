ace94ea2760254de38482dd5dc0299c718fab500
Dereje Amare <draglow21@gmail.com>
2023-12-11 19:25:21 +0300

    create navbar links and configure navigation links

--- feed/views.py
+++ feed/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'feed/home.html')
+def about(request):
+    return render(request,'feed/about.html')
+def service(request):
+    return render(request,'feed/service.html')
+def contact(request):
+    return render(request,'feed/contact.html')
+def car(request):
+    return render(request,'feed/car.html')
