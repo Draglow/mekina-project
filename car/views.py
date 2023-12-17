60fde6790e5788da646d8f79f697fa8bf7fef0e8
Dereje Amare <draglow21@gmail.com>
2023-12-17 23:52:19 +0300

    I create detail page for car app and add pagination page

--- car/views.py
+++ car/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Car
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 
 def cars(request):
-    return render(request,'car/cars.html')
+    cars=Car.objects.order_by('-created_date')
+    paginator=Paginator(cars,4)
+    page=request.GET.get('page')
+    car_page=paginator.get_page(page)
+    context={'cars':car_page}
+    return render(request,'car/cars.html',context)
+def detail_car(request,id):
+    detail=get_object_or_404(Car,pk=id)
+    context={'detail':detail}
+    return render(request,'car/detail.html',context)
 
 # Create your views here.
